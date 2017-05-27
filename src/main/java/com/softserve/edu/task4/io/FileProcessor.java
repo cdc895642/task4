4f4b6cb3f7e16ab00dbe9db69026c4cf9c52c10a
dev1 <dev1>
2017-05-27 23:17:40 +0300

    clean code

--- src/main/java/com/softserve/edu/task4/io/FileProcessor.java
+++ src/main/java/com/softserve/edu/task4/io/FileProcessor.java
@@ -1,7 +1,6 @@
 package com.softserve.edu.task4.io;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
